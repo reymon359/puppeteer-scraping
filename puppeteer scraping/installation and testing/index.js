6c5182c0c42bae47467b867621c3f9d7699766a9
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-09-17 07:49:05 +0200

    search on google and screenshot

--- puppeteer scraping/installation and testing/index.js
+++ puppeteer scraping/installation and testing/index.js
@@ -2,10 +2,13 @@ const puppeteer = require('puppeteer');
 
 (async() => {
     const browser = await puppeteer.launch({
-        headless: false // To see it lively on chromium
+        headless: false
     });
     const page = await browser.newPage();
     await page.goto('https://google.com');
+    await page.type('#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input', 'World', { delay: 100 }); // Types slower, like a user
+    await page.keyboard.press('Enter');
+    await page.waitForNavigation();
     await page.screenshot({ path: 'example.png' });
 
     // await browser.close();

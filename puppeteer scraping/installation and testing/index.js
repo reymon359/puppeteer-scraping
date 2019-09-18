6d45feb2744106969acb217c57f2f7232b91a15e
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-09-18 06:34:30 +0200

    fixed image path

--- puppeteer scraping/installation and testing/index.js
+++ puppeteer scraping/installation and testing/index.js
@@ -6,10 +6,10 @@ const puppeteer = require('puppeteer');
     });
     const page = await browser.newPage();
     await page.goto('https://google.com');
-    await page.type('#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input', 'World', { delay: 100 }); // Types slower, like a user
+    await page.type('#tsf > div:nth-child(2) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input', 'Harry Potter', { delay: 100 }); // Types slower, like a user
     await page.keyboard.press('Enter');
     await page.waitForNavigation();
-    await page.screenshot({ path: 'example.png' });
+    await page.screenshot({ path: './installation and testing/example.png' });
 
     // await browser.close();
 
